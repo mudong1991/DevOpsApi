59dcf3d465f93bbee986f390ce89fdedc6beb5bf
mudong@sefon.com <dong523554>
2017-11-13 21:31:42 +0800

    123123

--- firstapp/system_settings_views/serializers.py
+++ firstapp/system_settings_views/serializers.py
@@ -6,6 +6,8 @@ from rest_framework import serializers
 from urlparse import urlparse
 from django.conf import settings
 from firstapp import models
+from django.contrib.auth.hashers import make_password
+from django.core.validators import ValidationError
 from firstapp.util_aes import aes_api_data_encrypt, aes_html_data_decrypt
 from firstapp import cust_exceptions
 
@@ -16,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
     groups_name = serializers.SlugRelatedField(source='groups', many=True, slug_field='name', read_only=True)
 
     def create(self, validated_data):
-        print validated_data["password"]
+        validated_data["password"] = make_password(validated_data["password"])
         return super(UserSerializer, self).create(validated_data)
 
     class Meta:
